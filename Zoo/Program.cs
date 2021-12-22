50e55a7fee290b226104f07349279d94b3fa42a0
amyjprogrammer <amyjprogrammer@gmail.com>
2021-12-21 20:50:07 -0500

    Add project files.

---
+++ Zoo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Animal cat = new Animal(7.5);
+            Animal dog = new Animal(25.2);*/
+
+            Cat cat = new Cat();
+            Dog dog = new Dog(10);
+        }
+    }
+
+    //can't be instatiated..only inherited
+    abstract class Animal
+    {
+        public Animal(double weight)
+        {
+            Weight = weight;
+        }
+        public double Weight { get; set; }
+
+    }
+
+    class Cat : Animal
+    {
+        public Cat() :  base(4.5)
+        {
+
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog(double weight) : base(weight)
+        {
+
+        }
+    }
+}
